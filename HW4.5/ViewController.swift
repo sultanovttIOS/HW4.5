90d8597eb241e884e0e8c0eee02c600b4f53a120
sultanovttIOS <alisher192377@gmail.com>
2024-01-27 16:56:06 +0600

    Initial Commit

---
+++ HW4.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4.5
+//
+//  Created by Alisher Sultanov on 27/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
